059d8879f8687baaec783953b810582503fdbe5e
Anohros <anohros@gmx.net>
2022-11-27 14:09:11 +0100

    #7 updates FBX SDK from 2019.5 to 2020.3.1

--- external/fbxsdk/fbxsdk-config.cmake
+++ external/fbxsdk/fbxsdk-config.cmake
@@ -1,2 +1,2 @@
-set(FBXSDK_LIB "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2019.5/lib/vs2015/x86")
-set(FBXSDK_INCLUDE "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2019.5/include")
+set(FBXSDK_LIB "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.1/lib/vs2019/x86")
+set(FBXSDK_INCLUDE "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.1/include")
