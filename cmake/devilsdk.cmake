00bb933b048994ad631650a7d0a5259a7203ef48
Anohros <anohros@gmx.net>
2023-04-16 15:56:24 +0200

    Copies all dlls on post build.

--- cmake/devilsdk.cmake
+++ cmake/devilsdk.cmake
@@ -2,6 +2,7 @@ add_library(DevILSDK::DevIL SHARED IMPORTED)
 
 set_target_properties(DevILSDK::DevIL PROPERTIES
     IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/DevIL.lib"
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/DevIL.dll"
     INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_SOURCE_DIR}/external/devilsdk/include"
 )
 
@@ -9,6 +10,7 @@ add_library(DevILSDK::ILU SHARED IMPORTED)
 
 set_target_properties(DevILSDK::ILU PROPERTIES
     IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/ILU.lib"
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/ILU.dll"
     INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_SOURCE_DIR}/external/devilsdk/include"
 )
 
