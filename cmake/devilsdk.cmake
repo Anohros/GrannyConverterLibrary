ff3db440e81d47af3df4cae7f80ca8376f23e081
Anohros <anohros@gmx.net>
2026-05-31 01:42:46 +0200

    chore: update libraries to x64 libraries and corresponding configurations
    - Update DevIL library paths to point to the x64 directory.
    - Update FBX SDK library from 2020.3.1 VS2019 (x86) to 2020.3.9 VS2022 (x64).
    - Update FBX SDK paths to point to the x64 directory.
    - Update granny2.dll from x86 to x64 (granny2_x64.dll).

--- cmake/devilsdk.cmake
+++ cmake/devilsdk.cmake
@@ -1,16 +1,16 @@
 add_library(DevILSDK::DevIL SHARED IMPORTED)
 
 set_target_properties(DevILSDK::DevIL PROPERTIES
-    IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/DevIL.lib"
-    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/DevIL.dll"
+    IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x64/Release/DevIL.lib"
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x64/Release/DevIL.dll"
     INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_SOURCE_DIR}/external/devilsdk/include"
 )
 
 add_library(DevILSDK::ILU SHARED IMPORTED)
 
 set_target_properties(DevILSDK::ILU PROPERTIES
-    IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/ILU.lib"
-    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x86/Release/ILU.dll"
+    IMPORTED_IMPLIB "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x64/Release/ILU.lib"
+    IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/external/devilsdk/lib/x64/Release/ILU.dll"
     INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_SOURCE_DIR}/external/devilsdk/include"
 )
 
--- external/fbxsdk/fbxsdk-config.cmake
+++ external/fbxsdk/fbxsdk-config.cmake
@@ -1,2 +1,2 @@
-set(FBXSDK_LIB "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.1/lib/vs2019/x86")
-set(FBXSDK_INCLUDE "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.1/include")
+set(FBXSDK_LIB "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.9/lib/x64")
+set(FBXSDK_INCLUDE "C:/Program\ Files/Autodesk/FBX/FBX\ SDK/2020.3.9/include")
